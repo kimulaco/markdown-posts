b87c16f1d2171607f31739a30a734225c2b3edab
kimulaco <kmrkdev@gmail.com>
2019-04-27 17:46:08 +0900

    Update test dir

--- test/markdown-post-parser.test.js
+++ test/markdown-post-parser.test.js
@@ -1,9 +1,9 @@
 const MarkdownPostParser = require('../lib/markdown-post-parser')
 const markdownPostParser = new MarkdownPostParser({
-  main: 'README.md',
-  src: './example/src',
-  dist: './example/json/blog.json',
-  static: './example/static'
+  main: 'index.md',
+  src: './test/src',
+  dist: './test/json/blog.json',
+  static: './test/static'
 });
 
 markdownPostParser.generate();

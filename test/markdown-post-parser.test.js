0dbbc188f29125538f087c5b8c6ebccb5c4d11a7
kimulaco <kmrkdev@gmail.com>
2019-04-27 16:44:51 +0900

    Add tags

--- test/markdown-post-parser.test.js
+++ test/markdown-post-parser.test.js
@@ -2,7 +2,7 @@ const MarkdownPostParser = require('../lib/markdown-post-parser')
 const markdownPostParser = new MarkdownPostParser({
   main: 'README.md',
   src: './example/src',
-  dist: './example/json/posts.json',
+  dist: './example/json/blog.json',
   static: './example/static'
 });
 

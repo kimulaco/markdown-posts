fc34108cd2c1b54e4648c27ac108b7ca2cdd21a9
kimulaco <kmrkdev@gmail.com>
2019-04-28 12:03:12 +0900

    Update CLI

--- test/markdown-post-parser.test.js
+++
@@ -1,9 +0,0 @@
-const MarkdownPostParser = require('../lib/markdown-post-parser')
-const markdownPostParser = new MarkdownPostParser({
-  main: 'index.md',
-  src: './test/src',
-  dist: './test/json/blog.json',
-  static: './test/static'
-});
-
-markdownPostParser.generate();
